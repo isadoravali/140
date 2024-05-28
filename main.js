441e7195ce55a0114e5058874691a7bd9c3cabb3
isadoravali <126509584+isadoravali@users.noreply.github.com>
2024-05-28 14:52:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function setup() {
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.hide
+    video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded)
     poseNet.on('pose', gotPoses);
